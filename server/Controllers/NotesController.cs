c12dd4d5281aa5d578fdffbc88b7013329475e98
dwiszyns <dominikwisza@gmail.com>
2020-01-18 22:55:39 +0100

    Added rest of endpoints

--- server/Controllers/NotesController.cs
+++ server/Controllers/NotesController.cs
@@ -8,6 +8,8 @@ using NTR5.Models;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using System.Net.Mime;
+
 
 
 namespace NTR5.Controllers
@@ -30,10 +32,20 @@ namespace NTR5.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         public IActionResult Get(int page, DateTime dateTo, DateTime dateFrom, string category)
         {
-            //var notes = _context.Note.ToArray();
+            var allNotes = _context.Note.ToArray();
             var categories = _context.Category.ToArray();
-            PaginatedList<Note> list = new PaginatedList<Note>(_context.Note.ToList(),page,4);
-            return Ok(new { notes=list.ToList(), categories=categories, pager= new {currentPage=list.PageIndex, endPage=list.TotalPages }});
+            List<Note> notes = new List<Note> { };
+            foreach (var n in allNotes)
+            {
+                Note tmpNote = _context.Note.Include(i => i.NoteCategory).ThenInclude(noteCategories => noteCategories.IdcategoryNavigation).FirstOrDefault(note => note.Idnote == n.Idnote);
+                if (tmpNote.Date >= dateFrom && tmpNote.Date <= dateTo && (category == "" || tmpNote.NoteCategory.Where(m => m.IdcategoryNavigation.Name == category).Any()))
+                {
+                    notes.Add(n);
+                }
+            }
+
+            PaginatedList<Note> list = new PaginatedList<Note>(notes, page, 4);
+            return Ok(new { notes = list.ToList(), categories = categories, pager = new { currentPage = list.PageIndex, endPage = list.TotalPages } });
         }
 
         [HttpGet("{id}")]
@@ -41,18 +53,93 @@ namespace NTR5.Controllers
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult GetById(int? id)
         {
-            if (id == null) {
+            if (id == null)
+            {
                 return NotFound();
             }
-            Note note = _context.Note.Where(n=>n.Idnote==id).Include(n => n.NoteCategory).ThenInclude(nc => nc.IdcategoryNavigation).FirstOrDefault();
-            //return Ok(note);
-            return Ok(new {data=new NoteData(note)});
+            Note note = _context.Note.Where(n => n.Idnote == id).Include(n => n.NoteCategory).ThenInclude(nc => nc.IdcategoryNavigation).FirstOrDefault();
+            return Ok(new { data = new NoteData(note) });
         }
+
+        [HttpPost]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult Create([FromBody] NoteData note)
+        {
+            if (_context.Note.Any(n => n.Title == note.Title))
+            {
+                return StatusCode(400, "Note with title - " + note.Title + " - already exists");
+            }
+            Note newNote = new Note() { Title = note.Title, Description = note.Text, Date = note.Date, IsMarkdown = Convert.ToInt16(note.Markdown) };
+            _context.Note.Add(newNote);
+            _context.SaveChanges();
+            updateCategories(newNote.Idnote, note.NoteCategories);
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateException ex)
+            {
+                return StatusCode(500, ex.InnerException.Message);
+            }
+            return Ok();
+        }
+
         [HttpPut("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public IActionResult Update(int? id, [FromBody]Models.Note note)
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> UpdateAsync(int? id, [FromBody]NoteData note)
         {
+            Note oldNote = _context.Note.Where(n => n.Idnote == id).FirstOrDefault();
+            if (oldNote == null)
+            {
+                Note deletedNote = new Note();
+                await TryUpdateModelAsync(deletedNote);
+                return NotFound();
+            }
+            oldNote = _context.Note.Include(i => i.NoteCategory).ThenInclude(noteCategories => noteCategories.IdcategoryNavigation).FirstOrDefault(n => n.Idnote == oldNote.Idnote);
+            if (_context.Note.Where(n => n.Title == note.Title && n.Idnote != note.NoteID).Any())
+            {
+                return StatusCode(400, "Note with title - " + note.Title + " - already exists");
+            }
+            _context.Entry(oldNote).Property("Timestamp").OriginalValue = note.Timestamp;
+            if (await TryUpdateModelAsync<Note>(oldNote, "Note",
+                n => n.Title, n => n.Description, n => n.Date))
+            {
+                try
+                {
+                    await _context.SaveChangesAsync();
+                    updateCategories(oldNote.Idnote, note.NoteCategories);
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    var entry = ex.Entries.Single();
+                    var clientValues = (Note)entry.Entity;
+                    var databaseEntry = entry.GetDatabaseValues();
+                    if (databaseEntry == null)
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        var databaseValues = (Note)databaseEntry.ToObject();
+                        string error = getConcurrencyErrors(clientValues, databaseValues);
+                        return StatusCode(403, "The record you attempted to edit "
+                            + "was modified by another user after you got the original value. The "
+                            + "edit operation was canceled and the current values in the database "
+                            + "have been displayed. If you still want to edit this record, click "
+                            + "the Save button again. Otherwise click the Back to List hyperlink. Current values:\n" + error);
+                    }
+                }
+                catch (DbUpdateException ex)
+                {
+                    return StatusCode(500, ex.InnerException.Message);
+                }
+            }
             return Ok();
         }
 
@@ -61,7 +148,74 @@ namespace NTR5.Controllers
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult Delete(int? id)
         {
+            var note = _context.Note.Include(i => i.NoteCategory).ThenInclude(i => i.IdcategoryNavigation).FirstOrDefault(i => i.Idnote == id);
+            if (note == null)
+            {
+                return NotFound();
+            }
+
+            foreach (var category in note.NoteCategory)
+            {
+                _context.NoteCategory.Remove(category);
+                if (_context.NoteCategory.Where(i => i.Idcategory == category.IdcategoryNavigation.Idcategory).FirstOrDefault() == null)
+                {
+                    _context.Category.Remove(category.IdcategoryNavigation);
+                }
+
+            }
+            _context.Note.Remove(note);
+            _context.SaveChanges();
             return Ok();
+        }
+        private void updateCategories(int id, ICollection<String> NoteCategories)
+        {
+            Array.ForEach(NoteCategories.ToArray(), c =>
+            {
+                try
+                {
+                    Category category;
+                    using (var transaction = _context.Database.BeginTransaction())
+                    {
+                        var occurances = _context.Category.Where(categoryObj => categoryObj.Name == c).ToList();
+                        if (occurances.Count() == 0)
+                        {
+                            category = new Category { Name = c };
+                            _context.Category.Add(category);
+                            _context.SaveChanges();
+                        }
+                        else
+                        {
+                            category = occurances.FirstOrDefault();
+                        }
+                        _context.NoteCategory.Add(new NoteCategory
+                        {
+                            Idnote = id,
+                            Idcategory = category.Idcategory
+                        });
+                        _context.SaveChanges();
+                        transaction.Commit();
+                    }
+                }
+                catch (DbUpdateException ex)
+                {
+                    Console.WriteLine(ex.InnerException.Data);
+                }
+            });
+        }
+
+        private string getConcurrencyErrors(Note clientValues, Note databaseValues)
+        {
+            string error = "";
+            if (databaseValues.Title != clientValues.Title)
+                error += "Title" + "Current value: "
+                    + databaseValues.Title+"\n";
+            if (databaseValues.Description != clientValues.Description)
+                error += "Description" + "Current value: "
+                    + String.Format("{0:c}", databaseValues.Description)+"\n";
+            if (databaseValues.Date != clientValues.Date)
+                error += "NoteDate" + "Current value: "
+                    + String.Format("{0:d}", databaseValues.Date)+"\n";
+            return error;
         }
 
     }
